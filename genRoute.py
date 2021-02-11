c221dba3ff771a24acfcf0bbc9b819fbf3a34ea7
l0rddarkf0rce <l0rddarkf0rce@yahoo.com>
2021-02-11 00:40:25 -0500

    Add files via upload

--- genRoute.py
+++ genRoute.py
@@ -1,26 +1,89 @@
 ############################################################
 # Program Name:  genRoute.py
-# Version:       2.0
+# Version:       2.5
 # Author:        L0rd DarkF0ce
 # GitHub:        https://github.com/l0rddarkf0rce/genRoute
-# Date Created:  20210206
-# Last Modified: 20200209
+############################################################
+# 20210206
+#    Original program created
+#
+# 20200209
+#    Added code to create different files for each of the
+#    research tasks.
+#
+# 20200210
+#    Added code to calculate and print distance and cool
+#    down times.
 ############################################################
 
 import os
 import sys, getopt
+from math import sin, cos, sqrt, atan2, radians
 
 inFile = ''
-outFile = ''
 ERROR1 = 'Invalid number of parameters'
 ERROR2 = 'Invalid parameter provided'
 ERROR3 = 'ERROR: In file ({}) does not exists.'
-ERROR4 = 'ERROR: Out file ({}) already exists.'
+
+def coolDown(file):
+   coords = []
+   cd = {0: '15 sec',
+         1: '30 sec',
+         2: '1 min',
+         3: '1.5 min',
+         4: '1.5 min',
+         5: '2 min',
+         6: '3 min',
+         7: '5 min',
+         8: '6 min',
+         9: '6 min',
+         10: '7 min',
+         11: '7 min',
+         12: '8 min',
+         13: '8 min',
+         14: '8 min',
+         15: '9 min',
+         16: '9 min',
+         17: '9 min',
+         18: '10 min',
+         19: '10 min',
+         20: '11 min',
+         21: '11 min',
+         22: '12 min',
+         23: '12 min',
+         24: '13 min',
+         25: '14 min',
+         26: '15 min',
+         27: '15 min',
+         28: '15 min',
+         29: '16 min',
+         30: '16 min'}
+
+   with open(file, 'r') as f:
+      for line in f.readlines():
+         f_list = [float(i) for i in line.split(',') if i.strip()]
+         coords.append(f_list)
+
+   with open(file, 'w') as f:
+      # write back to the file
+      print(coords[0], file=f)
+      for x in range(1, len(coords)):
+         dist = distance(coords[x-1],coords[x])
+         print('{} distance {} km - cooldown {}'.format(coords[x], round(dist, 2), cd[round(dist, 0)]), file=f)
 
 def usage(name, msg):
-   print('ERROR: {}\n   Usage: {} -i <INPUTFILE> -o <OUTFILE>\n'.format(msg, name))
+   # Parameters:
+   #    name: string containing the name of the program itself
+   #    msg: string of the error message that we want to print
+   # Description:
+   #    Print usage message for our program
+   print('ERROR: {}\n   Usage: {} -i <INPUTFILE>\n'.format(msg, name))
 
 def trunc(fn):
+   # Parameters:
+   #    fn: filename that we want to truncate the last line for
+   # Description:
+   #    Removes the NL character from the last line of the file
    lines = open(fn, 'r').readlines()
    lastLine = (lines[-1].rstrip())
    lines[-1] = lastLine
@@ -28,12 +91,23 @@ def trunc(fn):
    return 0
 
 def find_nth(myString, mySubString, n):
+   # Parameters
+   #    myString: string that we will look in
+   #    mySubString: string that we will look for inside of myString
+   #    n: integer represent the # of the occurence of mySubString insode of myString
+   # Return:
+   #    If found, return the location of the nth occurence of mySubString inside of myString
    if (n == 1):
        return myString.find(mySubString)
    else:
        return myString.find(mySubString, find_nth(myString, mySubString, n -1) + 1)
 
 def findQuest(quest, questList):
+   # Parameters:
+   #    quest: string representing the quest that we are looking for
+   #    questList: Array of all of the available quests
+   # Return:
+   #    If found, the possition in the array
    n = -1
    for x in range(len(questList)):
        key = questList[x][0]
@@ -41,6 +115,29 @@ def findQuest(quest, questList):
        if quest == key:
            return x
    return n
+
+def distance(c1, c2):
+    # Parameters:
+    #    c1: tuple of float (lat, lon)
+    #    c2: tuple of float (lat, lon)
+    # Return:
+    #    Distance in km between c1 and c2: float
+    
+    # Earth's radius at the equator in KM
+    R = 6378.0
+
+    lat1, lon1 = c1
+    lat2, lon2 = c2
+    
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    
+    a = ((sin(dlat / 2)**2) + ((cos(radians(lat1)) * cos(radians(lat2))) * (sin(dlon / 2)**2)))
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    d = R * c
+    
+    return(d)
+
 
 def main():
    tmpFile = 'foobar'
@@ -87,6 +184,9 @@ def main():
             lat = line.split(' ')[1].split('"')[1]
             lon = line.split(' ')[2].split('"')[1]
             print("{},{}".format(lat,lon), file=f)
+      
+      # Calculate distance
+      coolDown(quest[0]+'.out')
 
       ret_code = os.system('del '+quest[0]+'.txt '+quest[0]+'.gpx')
       print('{} generated'.format(quest[0]+'.out'))
@@ -96,10 +196,6 @@ def filesOK():
 
    if not os.path.isfile(inFile):
       print(ERROR3.format(inFile))
-      code = False
-
-   if os.path.isfile(outFile):
-      print(ERROR4.format(outFile))
       code = False
 
    return code
